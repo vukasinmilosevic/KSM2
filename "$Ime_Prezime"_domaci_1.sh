f25dda234dd9870ad2ed0d04dec9e2f62966c319
emamaricic <119610911+emamaricic@users.noreply.github.com>
2022-12-04 09:28:12 +0100

    Add files via upload

---
+++ "$Ime_Prezime"_domaci_1.sh
@@ -0,0 +1,7 @@
+echo "Upisite ime osobe"
+read ime
+echo "Upisite prezime osobe"
+read prezime
+echo "Unesite datum rodjenja osobe"
+read birth_date
+echo "Ime: $ime, Prezime: $prezime, Datum Rodjenja: $birth_date">>HR_info.txt
